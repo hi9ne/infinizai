bfa104f82b7c95ac0cb97d3ddabb6eb70d7c2486
hi9ne <berdibaevchyngyz96@gmail.com>
2023-04-02 12:46:34 +0600

    first commit

---
+++ src/js/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel({
+        items:3,
+        loop: true,
+        autoplay: true,
+        autoplayTimeout: 5000,
+        autoplayHoverPause: true,
+    });
+});
